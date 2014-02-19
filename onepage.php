131c28088b5f99c5cc55ade9c00e9d1c65352e69
Brian Feeney <bcfeeney@gmail.com>
2014-02-19 10:17:14 -0500

    Added One Page
    For keeping the site contained to the landing page only

---
+++ onepage.php
@@ -0,0 +1,18 @@
+<?php global $more;?>
+
+<?php 
+/*
+Template Name: One Page
+*/
+get_header(); ?>
+
+<div class="container">
+	<div class="col-lg-8 col-lg-offset-2">
+		<section id="welcome">
+			<p>Brian Feeney is a designer in Brooklyn, NY, currently employed by the Brooklyn Museum.</p>
+		</section>
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
